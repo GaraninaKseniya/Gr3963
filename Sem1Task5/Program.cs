ba37f24d6d8fe7a9d548e0a12fd14f883df08df0
GaraninaKseniya <124591523+GaraninaKseniya@users.noreply.github.com>
2023-02-16 02:03:18 +0300

    task 5

---
+++ Sem1Task5/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача №5
+// Напишите программу, которая на вход принимает одно число (N), 
+// а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.Write("Введите число: ");
+string? lineNum = Console.ReadLine();
+if (lineNum != null)
+{
+  int num = int.Parse(lineNum);
+  string res = string.Empty;
+  int negNum = num * (-1);
+  while (negNum <= num)
+  {
+    res = res + negNum + "\t";
+    negNum++;
+  }
+  Console.WriteLine(res);
+}
