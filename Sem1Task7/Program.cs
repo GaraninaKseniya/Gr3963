c284f8ba189e27d839ddd1074dca53abfc27919a
GaraninaKseniya <124591523+GaraninaKseniya@users.noreply.github.com>
2023-02-16 02:52:22 +0300

    task 7

---
+++ Sem1Task7/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача №7
+// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает последнюю цифру этого числа.
+
+Console.Write("Введите число: ");
+string? lineNum = Console.ReadLine();
+if (lineNum != null)
+{
+  int num = int.Parse(lineNum);
+  if (num > 99 && num < 1000)
+  {
+    int res = num % 10;
+    Console.WriteLine("Последняя цифра числа: " + res);
+  }
+  else
+  {
+    Console.WriteLine("Введено не то число.");
+  }
+}
