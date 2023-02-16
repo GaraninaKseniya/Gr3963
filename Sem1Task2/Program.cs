c301aeb0c7c5d797cbba400ceaa5ad655a7247a0
GaraninaKseniya <124591523+GaraninaKseniya@users.noreply.github.com>
2023-02-16 22:01:36 +0300

    task 4 addition

--- Sem1Task2/Program.cs
+++ Sem1Task2/Program.cs
@@ -16,24 +16,3 @@ else
   Console.WriteLine("max = " + num2);
   Console.WriteLine("min = " + num1);
 }  
-
-// Console.Write("Введите число1: ");
-// string? numLine1 = Console.ReadLine();
-// Console.Write("Введите число2: ");
-// string? numLine2 = Console.ReadLine();
-// if (numLine1 != null && numLine2 != null)
-// {
-//   int num1 = int.Parse(numLine1);
-//   int num2 = int.Parse(numLine2);
-//   int max = num1;
-//   if (num2 > max)
-//   {
-//     Console.WriteLine("max = "+num2);
-//     Console.WriteLine("min = "+num1);
-//   }
-//   else
-//   {
-//     Console.WriteLine("max = "+num1);
-//     Console.WriteLine("min = "+num2);
-//   }
-// }  
--- Sem1Task4/Program.cs
+++ Sem1Task4/Program.cs
@@ -2,25 +2,19 @@
 // Напишите программу, которая принимает на вход три числа 
 // и выдаёт максимальное из этих чисел.
 
-Console.Write("Введите число1: ");
-string? numLine1 = Console.ReadLine();
-Console.Write("Введите число2: ");
-string? numLine2 = Console.ReadLine();
-Console.Write("Введите число3: ");
-string? numLine3 = Console.ReadLine();
-if (numLine1 != null && numLine2 != null && numLine3 != null)
+Console.Write("Введите число 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 3: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if (num2 > max)
 {
-  int num1 = int.Parse(numLine1);
-  int num2 = int.Parse(numLine2);
-  int num3 = int.Parse(numLine3);
-  int max = num1;
-  if (num2 > max)
-  {
-    max = num2;
-  }
-  if (num3 > max)
-  {
-    max = num3;
-  }
-  Console.WriteLine("max= " + max);
+  max = num2;
 }
+if (num3 > max)
+{
+  max = num3;
+}
+Console.WriteLine("max= " + max);
