396401de6add9509e7963337a27c99673c6d2be7
GaraninaKseniya <124591523+GaraninaKseniya@users.noreply.github.com>
2023-02-16 22:20:39 +0300

    task 8

--- Sem1Task8/Program.cs
+++ Sem1Task8/Program.cs
@@ -3,21 +3,8 @@
 // а на выходе показывает все чётные числа от 1 до N.
 
 Console.Write("Введите число: ");
-string? numLine = Console.ReadLine();
-if (numLine != null)
+int num = Convert.ToInt32(Console.ReadLine());
+for (int i = 2; i <= num; i = i + 2)
 {
-  double num = int.Parse(numLine);
-  string res = string.Empty;
-  double quotNum = num / num;
-  while (quotNum <= num)
-  {
-    double result = num / 2;
-    if (result % 2 == 0)
-    {
-    res = res + quotNum + "\t";
-    }
-    quotNum++;
-  }
-  Console.WriteLine(res);
+  Console.Write(i + " ");
 }
-
