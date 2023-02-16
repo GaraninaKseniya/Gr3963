bbdf21a5b74385817eb5da6a7c27aa46c6fc4bcb
GaraninaKseniya <124591523+GaraninaKseniya@users.noreply.github.com>
2023-02-16 21:48:04 +0300

    task 2 addition

--- Sem1Task2/Program.cs
+++ Sem1Task2/Program.cs
@@ -2,23 +2,38 @@
 // Напишите программу, которая на вход принимает два числа и выдаёт, 
 // какое число большее, а какое меньшее.
 
-Console.Write("Введите число1: ");
-string? numLine1 = Console.ReadLine();
-Console.Write("Введите число2: ");
-string? numLine2 = Console.ReadLine();
-if (numLine1 != null && numLine2 != null)
+Console.Write("Введите число 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
 {
-  int num1 = int.Parse(numLine1);
-  int num2 = int.Parse(numLine2);
-  int max = num1;
-  if (num2 > max)
-  {
-    Console.WriteLine("max = "+num2);
-    Console.WriteLine("min = "+num1);
-  }
-  else
-  {
-    Console.WriteLine("max = "+num1);
-    Console.WriteLine("min = "+num2);
-  }
+  Console.WriteLine("max = " + num1);
+  Console.WriteLine("min = " + num2);
+}
+else
+{
+  Console.WriteLine("max = " + num2);
+  Console.WriteLine("min = " + num1);
 }  
+
+// Console.Write("Введите число1: ");
+// string? numLine1 = Console.ReadLine();
+// Console.Write("Введите число2: ");
+// string? numLine2 = Console.ReadLine();
+// if (numLine1 != null && numLine2 != null)
+// {
+//   int num1 = int.Parse(numLine1);
+//   int num2 = int.Parse(numLine2);
+//   int max = num1;
+//   if (num2 > max)
+//   {
+//     Console.WriteLine("max = "+num2);
+//     Console.WriteLine("min = "+num1);
+//   }
+//   else
+//   {
+//     Console.WriteLine("max = "+num1);
+//     Console.WriteLine("min = "+num2);
+//   }
+// }  
---
+++ Sem1Task8/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 8.
+// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число: ");
+string? numLine = Console.ReadLine();
+if (numLine != null)
+{
+  double num = int.Parse(numLine);
+  string res = string.Empty;
+  double quotNum = num / num;
+  while (quotNum <= num)
+  {
+    double result = num / 2;
+    if (result % 2 == 0)
+    {
+    res = res + quotNum + "\t";
+    }
+    quotNum++;
+  }
+  Console.WriteLine(res);
+}
+
