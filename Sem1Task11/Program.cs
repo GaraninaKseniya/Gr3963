2d4ff2df371e9ddb32bd0b09dac42fb251bdc039
GaraninaKseniya <124591523+GaraninaKseniya@users.noreply.github.com>
2023-02-18 21:39:27 +0300

    task 11

---
+++ Sem1Task11/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача №11
+// Напишите программу, которая выводит случайное трёхзначное число 
+// и удаляет вторую цифру этого числа. 
+
+System.Random numSintezator = new System.Random();
+
+int rndNumber = numSintezator.Next(100, 1000);
+Console.WriteLine(rndNumber);
+// int firstNum = rndNumber / 100;
+// int lastNum = rndNumber % 10;
+// int num = firstNum*10 + lastNum;
+// Console.WriteLine(num);
+Console.WriteLine(rndNumber / 100 * 10 + rndNumber % 10);
