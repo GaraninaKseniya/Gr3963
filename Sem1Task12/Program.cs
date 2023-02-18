dc7de66414db5ce03b7023a51eca65c6521b59ca
GaraninaKseniya <124591523+GaraninaKseniya@users.noreply.github.com>
2023-02-18 22:22:58 +0300

    task 12

---
+++ Sem1Task12/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача №12
+// Напишите программу, которая будет принимать на вход два числа и выводить, 
+// является ли второе число кратным первому. Если второе число
+// некратно первому, то программа выводит остаток от деления. 
+
+Console.Write("Введите число 1: ");
+int firstNum = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите число 2: ");
+int secondNum = int.Parse(Console.ReadLine() ?? "0");
+int rem = firstNum % secondNum;
+if (rem == 0)
+{
+  Console.WriteLine("кратно");
+}
+else
+{
+  Console.WriteLine("не кратно, остаток "+rem);
+}
