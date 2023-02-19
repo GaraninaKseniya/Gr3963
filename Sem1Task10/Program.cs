e7ac1f58a838fc6493d60ccddd4ca28941b9c363
GaraninaKseniya <124591523+GaraninaKseniya@users.noreply.github.com>
2023-02-19 17:24:21 +0300

    task 10

---
+++ Sem1Task10/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 10: 
+// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введите трёхзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+// вариант 1
+if (num >= 100 && num < 1000)
+{
+  int num2 = num / 10 % 10;
+  Console.WriteLine($"Вторая цифра введенного числа: {num2}");
+  // вариант 2
+  char[] digits = num.ToString().ToCharArray();
+  Console.WriteLine($"Вторая цифра введенного числа: {int.Parse((digits[1]).ToString())}");
+}
+else
+{
+  Console.WriteLine("Введено не трехзначное число, повторите ввод.");
+}
