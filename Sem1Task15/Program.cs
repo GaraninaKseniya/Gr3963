3d73983537e7c591326b464ee97867f7fe72daf3
GaraninaKseniya <124591523+GaraninaKseniya@users.noreply.github.com>
2023-02-21 20:45:51 +0300

    task 15 addition

--- Sem1Task15/Program.cs
+++ Sem1Task15/Program.cs
@@ -19,3 +19,24 @@ else
 {
   Console.WriteLine("Это не день недели.");
 }
+
+Dictionary<int, string> daysOfWeek = new Dictionary<int, string>();
+// добавляем дни недели в словарь
+daysOfWeek.Add(1, "Понедельник, будний день");
+daysOfWeek.Add(2, "Вторник, будний день");
+daysOfWeek.Add(3, "Среда, будний день");
+daysOfWeek.Add(4, "Четверг, будний день");
+daysOfWeek.Add(5, "Пятница, будний день");
+daysOfWeek.Add(6, "Суббота, выходной");
+daysOfWeek.Add(7, "Воскресенье, выходной");
+// просим ввести день недели
+Console.Write("Введите день недели (1-7): ");
+int dayOfWeek = int.Parse(Console.ReadLine() ?? "0");
+if (daysOfWeek.ContainsKey(dayOfWeek))
+{
+  Console.WriteLine(daysOfWeek[dayOfWeek]);
+}
+else
+{
+  Console.WriteLine("Это не день недели.");
+}
