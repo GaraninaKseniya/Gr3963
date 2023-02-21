e24ab29abdc29e66365aafb4388623d473bb9b81
GaraninaKseniya <124591523+GaraninaKseniya@users.noreply.github.com>
2023-02-21 21:49:55 +0300

    task 13 addition

--- Sem1Task13/Program.cs
+++ Sem1Task13/Program.cs
@@ -2,12 +2,27 @@
 // Напишите программу, которая выводит третью цифру 
 // заданного числа или сообщает, что третьей цифры нет.
 
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// char[] array = num.ToString().ToCharArray();
+// if (num > 99)
+// {
+//   Console.WriteLine($"Третья цифра введенного числа: {int.Parse((array[2]).ToString())}");
+// }
+// else 
+// {
+//   Console.WriteLine("Третьей цифры нет.");
+// }
+
 Console.Write("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
-char[] array = num.ToString().ToCharArray();
 if (num > 99)
 {
-  Console.WriteLine($"Третья цифра введенного числа: {int.Parse((array[2]).ToString())}");
+  while (num > 999)
+  {
+    num/=10;
+  }
+  Console.WriteLine(num%10);
 }
 else 
 {
