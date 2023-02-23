24cbd7b11a518cc8000ced0b0650c3200564a26f
GaraninaKseniya <124591523+GaraninaKseniya@users.noreply.github.com>
2023-02-23 21:42:35 +0300

    task 13 addition

--- Sem1Task13/Program.cs
+++ Sem1Task13/Program.cs
@@ -2,18 +2,6 @@
 // Напишите программу, которая выводит третью цифру 
 // заданного числа или сообщает, что третьей цифры нет.
 
-// Console.Write("Введите число: ");
-// int num = Convert.ToInt32(Console.ReadLine());
-// char[] array = num.ToString().ToCharArray();
-// if (num > 99)
-// {
-//   Console.WriteLine($"Третья цифра введенного числа: {int.Parse((array[2]).ToString())}");
-// }
-// else 
-// {
-//   Console.WriteLine("Третьей цифры нет.");
-// }
-
 Console.Write("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 if (num > 99)
@@ -29,13 +17,13 @@ else
   Console.WriteLine("Третьей цифры нет.");
 }
 
-// Console.Write("Введите число: ");
-// char[] array = Console.ReadLine().ToString().ToCharArray();
-// if (array.Length > 2)
-// {
-//   Console.WriteLine($"Третья цифра введенного числа: {int.Parse((array[2]).ToString())}");
-// }
-// else 
-// {
-//   Console.WriteLine("Третьей цифры нет.");
-// }
+Console.Write("Введите число: ");
+char[] array = (Console.ReadLine() ??"0").ToString().ToCharArray();
+if (array.Length > 2)
+{
+  Console.WriteLine($"Третья цифра введенного числа: {int.Parse((array[2]).ToString())}");
+}
+else 
+{
+  Console.WriteLine("Третьей цифры нет.");
+}
