462f2f37959aba1846f5aaef9174d2a7d8510750
GaraninaKseniya <124591523+GaraninaKseniya@users.noreply.github.com>
2023-02-24 23:01:16 +0300

    task 19

---
+++ Sem3Task19/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача №19 
+// Напишите программу, которая принимает на вход пятизначное число 
+// и проверяет, является ли оно палиндромом.
+
+// метод читает данные от пользователя
+int ReadData(string msg)
+{
+  Console.WriteLine(msg);
+  return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// метод проверки 5-значного числа на палиндром
+bool PalinTest(int num)
+{
+  bool res = false;
+  if ((num / 10000 % 10 == num % 10) && (num / 1000 % 10 == num / 10 % 10))
+  {
+    res = true;
+    Console.WriteLine("Введенное число является палиндромом");
+  }
+  else
+  {
+    Console.WriteLine("Введенное число не является палиндромом");
+  }
+  return res;
+}
+
+int num = ReadData("Введите пятизначное число: ");
+PalinTest(num);
