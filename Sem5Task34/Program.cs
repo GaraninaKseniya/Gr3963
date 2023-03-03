1fd5434b8c47623b34cbfcfb84589ad8d3bcf9d9
GaraninaKseniya <124591523+GaraninaKseniya@users.noreply.github.com>
2023-03-03 04:48:13 +0300

    task 34

---
+++ Sem5Task34/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача №34 
+// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве. 
+
+// метод генерирует одномерный массив
+int[] Gen1DArr(int len, int min, int max)
+{
+  // блок корректировки входных значений
+  if (min > max)
+  {
+    int buf = min;
+    min = max;
+    max = buf;
+  }
+  int[] arr = new int[len];
+  for (int i = 0; i < arr.Length; i++)
+  {
+    arr[i] = new Random().Next(min, max + 1);
+  }
+  return arr;
+}
+
+// метод печатает одномерный массив
+void Print1DArr(int[] arr)
+{
+  Console.Write("[");
+  for (int i = 0; i < arr.Length - 1; i++)
+  {
+    Console.Write(arr[i] + ", ");
+  }
+  Console.Write(arr[arr.Length - 1]);
+  Console.WriteLine("]");
+}
+
+// метод нахождения количества чётных чисел в массиве
+int CountEvenNum(int[] arr)
+{
+  int count = 0;
+  for (int i = 0; i < arr.Length; i++)
+  {
+    if (arr[i] %2 == 0)
+    {
+      count++;
+    }
+  }
+  return count;
+}
+
+// метод выводит ответ пользователю
+void PrintData(string msg, int value)
+{
+  Console.WriteLine(msg + value);
+}
+
+int[] testArr = Gen1DArr(20, 100, 999);
+Print1DArr(testArr);
+PrintData("Количество чётных чисел в массиве: ", CountEvenNum(testArr));
