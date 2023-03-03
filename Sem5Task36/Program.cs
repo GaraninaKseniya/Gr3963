dd3b79af492bf630ff83547ae34dd6a21caac1ee
GaraninaKseniya <124591523+GaraninaKseniya@users.noreply.github.com>
2023-03-03 05:26:07 +0300

    task 36

---
+++ Sem5Task36/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача №36 
+// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях. 
+
+// метод генерирует одномерный массив
+int[] Gen1DArr(int len, int min, int max)
+{
+  // блок корректировки входных значений
+  if (min > max)
+  {
+    int buf = min;
+    min = max;
+    max = buf;
+  }
+  int[] arr = new int[len];
+  for (int i = 0; i < arr.Length; i++)
+  {
+    arr[i] = new Random().Next(min, max + 1);
+  }
+  return arr;
+}
+
+// метод печатает одномерный массив
+void Print1DArr(int[] arr)
+{
+  Console.Write("[");
+  for (int i = 0; i < arr.Length - 1; i++)
+  {
+    Console.Write(arr[i] + ", ");
+  }
+  Console.Write(arr[arr.Length - 1]);
+  Console.WriteLine("]");
+}
+
+// метод нахождения суммы элементов, стоящих на нечётных позициях
+int sumOddPosition(int[] arr)
+{
+  int sum = 0;
+  for (int i = 1; i < arr.Length; i = i + 2)
+  {
+    sum += arr[i];
+  }
+  return sum;
+}
+
+// метод выводит ответ пользователю
+void PrintData(string msg, int value)
+{
+  Console.WriteLine(msg + value);
+}
+
+int[] testArr = Gen1DArr(15, -10, 10);
+Print1DArr(testArr);
+int result = sumOddPosition(testArr);
+PrintData("Сумма элементов, стоящих на нечётных позициях, равна: ", result);
