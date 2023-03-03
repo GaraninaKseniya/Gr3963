441b3f0e49940ac9acfd1df10589c23913528751
GaraninaKseniya <124591523+GaraninaKseniya@users.noreply.github.com>
2023-03-03 05:01:59 +0300

    task 34

--- Sem5Task34/Program.cs
+++ Sem5Task34/Program.cs
@@ -1,6 +1,7 @@
 ﻿// Задача №34 
 // Задайте массив заполненный случайными положительными трёхзначными числами. 
 // Напишите программу, которая покажет количество чётных чисел в массиве. 
+// * Отсортировать массив методом пузырька 
 
 // метод генерирует одномерный массив
 int[] Gen1DArr(int len, int min, int max)
@@ -38,7 +39,7 @@ int CountEvenNum(int[] arr)
   int count = 0;
   for (int i = 0; i < arr.Length; i++)
   {
-    if (arr[i] %2 == 0)
+    if (arr[i] % 2 == 0)
     {
       count++;
     }
@@ -52,6 +53,26 @@ void PrintData(string msg, int value)
   Console.WriteLine(msg + value);
 }
 
+// метод сортировки массива "пузырьком"
+static int[] BubbleSort(int[] mas)
+{
+  int temp;
+  for (int i = 0; i < mas.Length; i++)
+  {
+    for (int j = i + 1; j < mas.Length; j++)
+    {
+      if (mas[i] > mas[j])
+      {
+        temp = mas[i];
+        mas[i] = mas[j];
+        mas[j] = temp;
+      }
+    }
+  }
+  return mas;
+}
+
 int[] testArr = Gen1DArr(20, 100, 999);
 Print1DArr(testArr);
+Print1DArr(BubbleSort(testArr));
 PrintData("Количество чётных чисел в массиве: ", CountEvenNum(testArr));
