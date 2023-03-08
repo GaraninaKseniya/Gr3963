1611f611780749177adc11d1e232dfc52c9ac285
GaraninaKseniya <124591523+GaraninaKseniya@users.noreply.github.com>
2023-03-09 02:31:15 +0300

    task 42

---
+++ Sem6Task42/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача №42
+// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+// метод читает данные от пользователя
+int ReadData(string msg)
+{
+  Console.WriteLine(msg);
+  return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// метод преобразовывает десятичное число в двоичное
+string DecToBin(int num)
+{
+  string res = string.Empty;
+  while (num > 0)
+  {
+    res = num % 2 + res;
+    num = num / 2;
+  }
+  return res;
+}
+
+// метод выводит ответ пользователю
+void PrintData(string msg)
+{
+  Console.WriteLine(msg);
+}
+
+int number = ReadData("Введите десятичное число: ");
+PrintData($"Преобразование в двоичное число : {DecToBin(number)}");
