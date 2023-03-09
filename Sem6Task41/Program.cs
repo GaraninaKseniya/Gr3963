081f8afdbc137c1e14e403b75e6a8bfaec4aad8e
GaraninaKseniya <124591523+GaraninaKseniya@users.noreply.github.com>
2023-03-09 21:21:57 +0300

    task 41

--- Sem6Task41/Program.cs
+++ Sem6Task41/Program.cs
@@ -1,3 +1,32 @@
 ﻿// Задача №41 
 // Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь. 
 
+// метод читает данные от пользователя
+int ReadData(string msg)
+{
+  Console.WriteLine(msg);
+  return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// метод подсчета вводимых с клавиатуры чисел (> 0)
+int countNum(int numM)
+{
+  int res = 0;
+  for (int i = 0; i < numM; i++)
+  {
+    if (ReadData("") > 0)
+    {
+      res++;
+    }
+  }
+  return res;
+}
+
+// метод выводит ответ пользователю
+void PrintData(string msg)
+{
+  Console.WriteLine(msg);
+}
+
+int numberM = ReadData("Введите количество чисел: ");
+PrintData($"Введено {countNum(numberM)} чисел больше 0.");
