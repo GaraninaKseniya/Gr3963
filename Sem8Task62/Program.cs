1f75bc201187b9de5d5d345d3041ec0610fe91f1
GaraninaKseniya <124591523+GaraninaKseniya@users.noreply.github.com>
2023-03-14 00:32:30 +0300

    task 62

--- Sem8Task62/Program.cs
+++ Sem8Task62/Program.cs
@@ -1,3 +1,35 @@
 ﻿// Задача №62 
 // Напишите программу, которая заполнит спирально массив 4 на 4.
 
+// метод генерирует двумерный массив
+void GenSpiral2DArr(int[,] arr)
+{
+  int temp = 1;
+  int i = 0; int j = 0;
+  while (temp <= arr.GetLength(0) * arr.GetLength(1))
+  {
+    arr[i, j] = temp;
+    temp++;
+    if (i <= j + 1 && i + j < arr.GetLength(1) - 1) j++;
+    else if (i < j && i + j >= arr.GetLength(0) - 1) i++;
+    else if (i >= j && i + j > arr.GetLength(1) - 1) j--;
+    else i--;
+  }
+}
+
+// метод печатает двумерный массив
+void Print2DArr(int[,] arr)
+{
+  for (int i = 0; i < arr.GetLength(0); i++)
+  {
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+      Console.Write($"{arr[i, j]}\t");
+    }
+    Console.WriteLine();
+  }
+}
+
+int[,] testArr = new int[4, 4];
+GenSpiral2DArr(testArr);
+Print2DArr(testArr);
