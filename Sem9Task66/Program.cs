14c967c9633db8da8ddd45dbe078350a704cc086
GaraninaKseniya <124591523+GaraninaKseniya@users.noreply.github.com>
2023-03-17 02:28:44 +0300

    task 66

---
+++ Sem9Task66/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача №66 
+// Задайте значения M и N. Напишите программу, которая найдёт сумму 
+// натуральных элементов в промежутке от M до N.
+
+// метод читает данные от пользователя
+int ReadData(string msg)
+{
+  Console.WriteLine(msg);
+  return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// метод выводит сумму натуральных элементов в промежутке с помощью рекурсии
+int RecSumElem(int m, int n)
+{
+  int res = 0;
+  if (m >= n) return res = n;
+  else return res + m + RecSumElem(m + 1, n);
+}
+
+// метод выводит ответ пользователю
+void PrintData(string msg)
+{
+  Console.WriteLine(msg);
+}
+
+int numberM = ReadData("Введите число M:");
+int numberN = ReadData("Введите число N:");
+int result = numberM < numberN ? RecSumElem(numberM, numberN) : RecSumElem(numberN, numberM);
+PrintData($"Сумма натуральных элементов в промежутке = {result}");
