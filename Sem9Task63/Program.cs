f8038a25533b1668a5f007a8cf6ca9870efb2b42
GaraninaKseniya <124591523+GaraninaKseniya@users.noreply.github.com>
2023-03-16 23:48:24 +0300

    task 63

---
+++ Sem9Task63/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача №63
+// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке
+// от N до 1. Выполнить с помощью рекурсии.
+
+// метод читает данные от пользователя
+int ReadData(string msg)
+{
+  Console.WriteLine(msg);
+  return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// метод выводит все натуральные числа в промежутке с помощью рекурсии
+void LineGenRec(int num)
+{
+  Console.Write(num + " ");
+  if (num == 1)
+  {
+
+  }
+  else
+  {
+  LineGenRec(num-1);
+  }
+}
+
+int number = ReadData("Введите число N:");
+LineGenRec(number);
