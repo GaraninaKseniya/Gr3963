145074bcafbabc66977511ab005649f693cf8697
GaraninaKseniya <124591523+GaraninaKseniya@users.noreply.github.com>
2023-03-18 02:59:41 +0300

    task 38

--- Sem5Task38/Program.cs
+++ Sem5Task38/Program.cs
@@ -2,14 +2,15 @@
 // Задайте массив вещественных чисел. Найдите разницу между 
 // максимальным и минимальным элементом массива. 
 
-// метод генерирует и печатает одномерный массив вещественных чисел
-double[] Gen1DArr(int len)
+// метод генерирует (и печатает) одномерный массив вещественных чисел
+double[] Gen1DArr(int len, int minElem, int maxElem)
 {
   double[] arr = new double[len];
   for (int i = 0; i < arr.Length; i++)
   {
-    arr[i] = new Random().NextDouble() * 100;
-    Console.Write("{0,7:F1}", arr[i]);
+    arr[i] = new Random().Next(minElem, maxElem + 1) + (new Random().NextDouble());
+    //Console.Write("{0,7:F1}", arr[i]);
+    arr[i] = Math.Round(arr[i], 2);
   }
   return arr;
 }
@@ -27,13 +28,25 @@ double MaxMinDiff(double[] arr)
   return (max - min);
 }
 
+// метод печатает одномерный массив
+void Print1DArr(double[] arr)
+{
+  Console.Write("[");
+  for (int i = 0; i < arr.Length - 1; i++)
+  {
+    Console.Write(arr[i] + ", ");
+  }
+  Console.Write(arr[arr.Length - 1]);
+  Console.WriteLine("]");
+}
+
 // метод выводит ответ пользователю
 void PrintData(string msg)
 {
   Console.WriteLine(msg);
 }
 
-double[] testArr = Gen1DArr(10);
-Console.WriteLine();
+double[] testArr = Gen1DArr(100, 0, 50);
+Print1DArr(testArr);
 double result = MaxMinDiff(testArr);
 PrintData($"Разница между максимальным и минимальным элементом массива равна:{result}");
